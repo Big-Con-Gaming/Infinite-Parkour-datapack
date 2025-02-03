079c1be297210872909ad94db3695c94c1cb6585
Big_Con_Gaming <connorismetotally@gmail.com>
2025-02-02 21:50:57 -0500

    Full Implementation of JSON
    So, great news! There's a new version of the compiler! This will insert all JSON files as data merge storage commands at the end of load.mcfunction on every compile! This took me a whole weekend, but I'm so excited for it to be done! If there's any bugs, don't hesitate to let me know!
    
    Also, the block dictionary is filled out with every currently implemented block.
    
    Next, I will make sure all references of blocks use dictionary searches!

--- autobuild.bat
+++ autobuild.bat
@@ -1,3 +1,3 @@
 @echo off
 rmdir "./data/infinite_parkour/function" /s /q
-java -jar ./MinecraftScript.jar ./src ./data/infinite_parkour/function infinite_parkour 2 .
+java -jar ./MinecraftScriptJSON.jar ./src ./data/infinite_parkour/function infinite_parkour 2 .
