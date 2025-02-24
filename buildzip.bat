8170945537592ae3e5df16c19a9773e55222955d
Big_Con_Gaming <connorismetotally@gmail.com>
2025-02-24 17:19:34 -0500

    New Compiler Options
    build.bat has now changed to no longer generate a zip file, that functionality has now moved over to buildzip.bat. This change was needed to make a part of the auto updater program work.

--- buildzip.bat
+++ buildzip.bat
@@ -1,4 +1,5 @@
 @echo off
 rmdir "./data/infinite_parkour/function" /s /q
 java -jar ./MinecraftScriptJSON.jar ./src ./data/infinite_parkour/function infinite_parkour 2
-echo Complete!
+echo Zipping...
+tar -acf "Infinite Parkour.zip" data pack.mcmeta
